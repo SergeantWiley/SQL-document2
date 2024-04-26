56e3172de18d7e3df8e0b5537611b1211e26e70e
Corporal Rat <86330761+SergeantWiley@users.noreply.github.com>
2024-04-26 08:19:16 -0700

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -2,12 +2,12 @@ CREATE TABLE customers (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(255)
 );
-
+-- Customers table
 CREATE TABLE theaters (
     theater_id INT PRIMARY KEY,
     theater_name VARCHAR(255)
 );
-
+-- Theaters table
 CREATE TABLE tickets (
     ticket_id INT PRIMARY KEY,
     theater_id INT,
@@ -16,7 +16,7 @@ CREATE TABLE tickets (
     FOREIGN KEY (theater_id) REFERENCES theaters(theater_id),
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
-
+-- tickets table. theater id and customer id is dependant on theater id
 CREATE table movies (
     movie_id INT PRIMARY KEY,
     movie_name VARCHAR(255),
