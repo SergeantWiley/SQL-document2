cf909c6a522c05f0c387258d6ef424ffe25f439e
Corporal Rat <86330761+SergeantWiley@users.noreply.github.com>
2024-04-26 08:23:42 -0700

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -1,3 +1,7 @@
+-- All tables are depedant on the customer.
+-- The theater and table is dependant on the customer
+-- The movie is dependant of the theater
+-- The consessions is only dependant on the customer
 CREATE TABLE customers (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(255)
@@ -16,16 +20,17 @@ CREATE TABLE tickets (
     FOREIGN KEY (theater_id) REFERENCES theaters(theater_id),
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
--- tickets table. theater id and customer id is dependant on theater id
+-- tickets table. theater id and customer id is dependant on theater table and customres table respectively
 CREATE table movies (
     movie_id INT PRIMARY KEY,
     movie_name VARCHAR(255),
     price FLOAT,
     air_time TIME
 );
-
+-- movie table
 CREATE TABLE concessions (
     concession_id INT PRIMARY KEY,
     item_name VARCHAR(255),
     price FLOAT
 );
+-- Concessions table
